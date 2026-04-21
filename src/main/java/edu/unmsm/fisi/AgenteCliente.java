194e826ea6fae9279a9460fc60b4a24837e188dc
RichardTorresRivera <rmtr2711@gmail.com>
2026-04-21 15:39:53 -0500

    update: agregar argumento de pedido a cliente

--- src/main/java/edu/unmsm/fisi/AgenteCliente.java
+++ src/main/java/edu/unmsm/fisi/AgenteCliente.java
@@ -1,7 +1,6 @@
 package edu.unmsm.fisi;
 
 import jade.core.Agent;
-import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
@@ -12,30 +11,35 @@ import jade.lang.acl.ACLMessage;
 
 public class AgenteCliente extends Agent {
     protected void setup() {
-        System.out.printf("[Cliente: %s] - Informacion del cliente: %n", getLocalName());
-        System.out.printf("- Nombre global: %s%n", getAID().getName());
-        System.out.println("------------------------------------------");
-        ServiceDescription cliente_servicio = new ServiceDescription();
-        cliente_servicio.setType("restaurante-service");
-        buscarServicioRestaurante(cliente_servicio);
+        // Capturar pedido
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            String pedido = (String) args[0];
 
-
-        addBehaviour(new CyclicBehaviour() {
-            @Override
-            public void action() {
-                ACLMessage msg = receive();
-                if (msg != null) {
-                    System.out.println("[Cliente: %s] - Mensaje recibido");
-                    System.out.printf("- Emisor: %s", msg.getSender().getName());
-                    System.out.printf("- Mensaje: %s", msg.getContent());
-                    System.out.println("------------------------------------------");
+            System.out.printf("[Cliente: %s] - Informacion del cliente: %n", getLocalName());
+            System.out.printf("- Nombre global: %s%n", getAID().getName());
+            System.out.println("------------------------------------------");
+            ServiceDescription cliente_servicio = new ServiceDescription();
+            cliente_servicio.setType("restaurante-service");
+            buscarServicioRestaurante(cliente_servicio, pedido);
+            addBehaviour(new CyclicBehaviour() {
+                @Override
+                public void action() {
+                    ACLMessage msg = receive();
+                    if (msg != null) {
+                        System.out.println("[Cliente: %s] - Mensaje recibido");
+                        System.out.printf("- Emisor: %s", msg.getSender().getName());
+                        System.out.printf("- Mensaje: %s", msg.getContent());
+                        System.out.println("------------------------------------------");
+                    }
+                    else block();
                 }
-                else block();
-            }
-        });
+            });
+        }
+
     }
 
-    protected void buscarServicioRestaurante(ServiceDescription sd){
+    protected void buscarServicioRestaurante(ServiceDescription sd, String pedido){
         addBehaviour(new TickerBehaviour(this, 5000) {
             @Override
             protected void onTick() {
@@ -47,7 +51,7 @@ public class AgenteCliente extends Agent {
                     if (res.length != 0){
                         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
                         msg.addReceiver(res[0].getName());
-                        msg.setContent("Solicito un pedido");
+                        msg.setContent(pedido);
                         myAgent.send(msg);
                         stop();
                     } else {
@@ -61,6 +65,6 @@ public class AgenteCliente extends Agent {
     }
 
     protected void takeDown(){
-        System.out.printf("[Cliente: %s] - Cliente se despide", getLocalName());
+        System.out.printf("[Cliente: %s] - Cliente se despide%n", getLocalName());
     }
 }
